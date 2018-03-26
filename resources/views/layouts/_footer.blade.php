894afe7bf0c9613147d97930228b2ee511dce583
sunxyw <xy2496419818@gmail.com>
2018-03-26 17:17:22 +0800

    搭建基础框架

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Copyright &copy; <a>Build-Dragon</a>. All Rights Reserved.
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
