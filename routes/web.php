5467a804e0fd22bde89f47a70141f33151f528a5
sunxyw <xy2496419818@gmail.com>
2018-03-26 17:51:45 +0800

    用户资料页面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
