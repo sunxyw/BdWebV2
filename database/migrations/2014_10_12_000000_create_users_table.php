8e3397cb3a94eb89ff0a2e075814790deeb8840e
sunxyw <xy2496419818@gmail.com>
2018-03-26 18:02:09 +0800

    基本资料编辑

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name', 80);
             $table->string('email', 80)->unique();
             $table->string('password', 80);
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
