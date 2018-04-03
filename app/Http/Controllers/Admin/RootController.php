3f993b0986fdb1d674f440e20217ac9e79d798e5
sunxyw <xy2496419818@gmail.com>
2018-04-03 17:03:31 +0800

    增加后台页面，使用Vali-Admin

--- app/Http/Controllers/Admin/RootController.php
+++ app/Http/Controllers/Admin/RootController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Project;
